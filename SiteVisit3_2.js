722ea538cdf6c22b34494a166e9e41c241395e42
sallycariveau <149625414+sallycariveau@users.noreply.github.com>
2024-04-28 07:52:35 -0600

    Add files via upload

---
+++ SiteVisit3_2.js
@@ -0,0 +1,18 @@
+var ramps = {
+"type": "FeatureCollection",
+"name": "sitevisit",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 1, "Name": "Kevin Zimmerman", "Contact_Info": "(303)506-5633, 43605 Elk Rv Rd SS, CO", "SECTION": " Elk River Visit", "AGENCY": " ", "ADMIN": " ", "DESIG": " ", "STATE_LGD": " ", " Vessels": " ", "FeatureID": "{0C33EB83-08B8-4741-BB40-B7FCC7BD8B4A}" }, "geometry": { "type": "Point", "coordinates": [ -106.8922530, 40.5534516 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 2, "Name": "Jay Fetcher", "Contact_Info": "beyler93@gmail .com Liz Beyler-ranch manager", "SECTION": "Elk River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -106.9410058, 40.6896711 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 3, "Name": "Sandy Huber", "Contact_Info": "sandyjmcclung@gmail.com 28874 CR 64", "SECTION": "Elk River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -106.8769188, 40.7310853 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 4, "Name": "Doug Monger", "Contact_Info": "(970)846-4523", "SECTION": "Middle Yampa River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -107.2009614, 40.5169639 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 5, "Name": "Whitney", "Contact_Info": "klwhitney79@gmail.com", "SECTION": "Middle Yampa River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -107.1472758, 40.4830463 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 6, "Name": "Merrill", "Contact_Info": "coloradocranes@gmail.com", "SECTION": "Middle Yampa River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -107.2100362, 40.5115684 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 7, "Name": "Hayden Access Site", "Contact_Info": "mathew.mendisco@haydencolorado.org", "SECTION": "Middle Yampa River Visit", "AGENCY": "BLM", "ADMIN": "BLM", "DESIG": "N\/A", "STATE_LGD": "Bureau of Land Management", "Vessels": "float", "FeatureID": "{D1BC8DF4-B783-4D02-BC25-B3301BF46984}" }, "geometry": { "type": "Point", "coordinates": [ -107.2638548391825,40.501627076266281 ] } },
+
+
+
+
+]
+}
